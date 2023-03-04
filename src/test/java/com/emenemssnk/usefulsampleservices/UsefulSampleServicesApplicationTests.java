2bd5abbc2ed7dd14d3c1a123d788e669c4e2bb2b
Michal Dziedzic <michaldziedzic@MacBook-Air-Michal.local>
2023-03-05 00:08:08 +0100

    Initial commit

---
+++ src/test/java/com/emenemssnk/usefulsampleservices/UsefulSampleServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emenemssnk.usefulsampleservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulSampleServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
